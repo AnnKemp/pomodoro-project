7d12c60c45d614d60a17dea978b9ed2499f3cad8
AnnKemp <ann.kemp@scarlet.be>
2020-04-08 23:37:55 +0200

    updaten

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ function App() {
         <p>Lets render this to the DOM</p>
       </div>
   );
-
+    const element = React.createElement(
+        'h1',
+        {className: 'greeting'},
+        'Dadada!'
+    );
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>Lets render this to the DOM</p>
-      {JSX}
+      {JSX}<br />{element}
       <button onClick={() => this.setState({ liked: true })}>
         Like
       </button>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
 }
-
+setInterval(App, 1000);
 export default App;
+
