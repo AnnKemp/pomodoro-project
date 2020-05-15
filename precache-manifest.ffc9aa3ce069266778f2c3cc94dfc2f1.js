dd8608a833dc87233d04ca4bb2845a3734b407b3
AnnKemp <ann.kemp@scarlet.be>
2020-05-15 15:31:52 +0200

    Updates

---
+++ precache-manifest.ffc9aa3ce069266778f2c3cc94dfc2f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cbdb693a23094fc8d4ce527095f74a",
+    "url": "/pomodoro-project/index.html"
+  },
+  {
+    "revision": "1fce97a772d0208ab6fa",
+    "url": "/pomodoro-project/static/css/main.1bf12f3d.chunk.css"
+  },
+  {
+    "revision": "ef442f49b46af8f1a53f",
+    "url": "/pomodoro-project/static/js/2.adb75af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-project/static/js/2.adb75af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fce97a772d0208ab6fa",
+    "url": "/pomodoro-project/static/js/main.1e3b3828.chunk.js"
+  },
+  {
+    "revision": "4c8e20592aad46431c1e",
+    "url": "/pomodoro-project/static/js/runtime-main.d3e33ec4.js"
+  }
+]);
